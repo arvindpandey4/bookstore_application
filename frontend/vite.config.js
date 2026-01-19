678d14e8d3ea8445b95f7853f0306ffcdc134fed
Arvind Pandey <114512882+arvindpandeysrm@users.noreply.github.com>
2026-01-19 13:58:40 +0530

    fix: css cleanup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     preprocessorOptions: {
       scss: {
         api: 'modern-compiler',
-        silenceDeprecations: ['import', 'global-builtin', 'color-functions', 'mixed-decls'],
+        silenceDeprecations: ['import', 'global-builtin', 'color-functions', 'if-function'],
       },
     },
   },
